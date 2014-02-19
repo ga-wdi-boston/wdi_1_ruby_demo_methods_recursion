7dcaa4a14625edacd1d7d1f4c6410b11550a2e6a
David Fisher <tibbon@gmail.com>
2014-02-19 16:41:50 -0500

    Recursion demo

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ruby Recusion Demo
+
+Demonstrate the usage of recursion to calculate factorial with Ruby.
+
+Note that a recursion must have a 'base case' to resolve the call stack and not create an error.
+
+Wikipedia on [Recursion](http://en.wikipedia.org/wiki/Recursion_(computer_science))
---
+++ recursion.rb
@@ -0,0 +1,18 @@
+require 'pry'
+
+def factorial(number)
+	#binding.pry
+	if number > 1
+		return number * factorial(number - 1)
+	else
+		return 1
+	end
+end
+
+# Alternative syntax using terinary operator for the if/else
+def terinary_factorial(number)
+  number > 1 ? number * terinary_factorial(number - 1) : 1
+end
+
+result = factorial(5)
+puts result
