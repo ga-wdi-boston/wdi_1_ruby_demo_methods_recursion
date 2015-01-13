dc99e1d99cfbc3eb01cb42e8a8b6149006703f1a
Anna Tsykalova <tsyan@users.noreply.github.com>
2015-01-13 07:36:19 -0500

    Update factorial.rb

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -12,5 +12,5 @@ end
 # Alternative syntax using ternary operator for the if/else
 # Exact same code as above otherwise
 def ternary_factorial(number)
-  number > 1 ? number * terinary_factorial(number - 1) : 1
+  number > 1 ? number * ternary_factorial(number - 1) : 1
 end
